7bd3897845f5e8b72fb873e38cfe4a49811d4ad5
yeliwei01994 <925727197@qq.com>
2026-07-10 11:00:03 +0800

    feat: add article reading and search experience

---
+++ src/pages/search-index.json.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from 'astro';
+import { getPublishedPosts } from '../lib/content';
+
+export const prerender = true;
+
+export const GET: APIRoute = async () => {
+  const posts = await getPublishedPosts();
+  return new Response(JSON.stringify(posts.map((post) => ({
+    title: post.data.title,
+    description: post.data.description,
+    href: `/posts/${post.id}/`,
+    tags: post.data.tags,
+  }))), { headers: { 'Content-Type': 'application/json; charset=utf-8' } });
+};
